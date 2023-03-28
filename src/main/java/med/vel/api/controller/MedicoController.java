178e391e3f2c7747422c2d149afad5bc6003cbfa
IgorHma <55795539+IgorHma@users.noreply.github.com>
2023-03-27 23:11:14 -0300

    [IgorHma][Medvoll] - Spring Data JPA

--- src/main/java/med/vel/api/controller/MedicoController.java
+++ src/main/java/med/vel/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package med.vel.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.vel.api.medico.DadosCadastrosMedico;
+import med.vel.api.medico.Medico;
+import med.vel.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
-    public void cadastrarMedicos(@RequestBody DadosCadastrosMedico dados){
+    @Transactional
+    public void cadastrarMedicos(@RequestBody @Valid DadosCadastrosMedico dados){
         System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
--- src/main/java/med/vel/api/medico/DadosCadastrosMedico.java
+++ src/main/java/med/vel/api/medico/DadosCadastrosMedico.java
@@ -1,6 +1,27 @@
 package med.vel.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.vel.api.endereco.DadosEndereco;
 
-public record DadosCadastrosMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco ) {
+public record DadosCadastrosMedico(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
+
 }
