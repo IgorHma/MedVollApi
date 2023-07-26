570fe258f340c9d476a79f44392a0fc5861aec6e
IgorHma <55795539+IgorHma@users.noreply.github.com>
2023-07-26 19:53:12 -0300

    [Igor] - Incluindo e manipulando JSON Web Token

--- src/main/java/med/vel/api/controller/AutenticacaoController.java
+++ src/main/java/med/vel/api/controller/AutenticacaoController.java
@@ -2,10 +2,14 @@ package med.vel.api.controller;
 
 import jakarta.validation.Valid;
 import med.vel.api.domain.usuario.DadosAutenticacao;
+import med.vel.api.domain.usuario.Usuario;
+import med.vel.api.infra.security.DadosTokenJWT;
+import med.vel.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +21,17 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
+
 }
---
+++ src/main/java/med/vel/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package med.vel.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
